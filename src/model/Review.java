0fa5781ba5f5022bfc995fab2c8ee0a0294eab48
levonk@uw.edu <levonk@uw.edu>
2013-05-08 18:52:19 +0000

    Levon Kechichian TCSS 360 - Software Development and Quality Assurance 8 May, 2013 Group Project
    Finished:
    
    Created the Review model class outline.
    
    In Progress:
    
    Need to include the class fields (representational data).
    Need to include the class methods (behaviors).
    
    Comment:
    
    This is just the bare bones class structure plus a class comment.
    Enjoy!
    ♥♦♣♠

---
+++ src/model/Review.java
@@ -0,0 +1,13 @@
+package model;
+
+/**
+ * Class to encapsulate a Review in our system.
+ * (This Review is based on a Paper Object)
+ * 
+ * @author Levon Kechichian
+ * @version 1.0 (Spring 2013)
+ */
+public class Review 
+{
+	
+}
