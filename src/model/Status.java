76df4f05c1d30a7c96abeaebbb7aee02c7dd4fd2
Danielle Tucker <danielle@tuckerlabs.com>
2013-05-14 18:00:55 +0000

    Added two enumeration classes for Role and Status and filled in some of the classes for the User class.

---
+++ src/model/Status.java
@@ -0,0 +1,16 @@
+package model;
+
+/**
+ * Describes the workflow status of a paper to be reviewed.
+ * @author Danielle Tucker
+ * @version 14 May 2013
+ */
+public enum Status {
+	PROGRAM_CHAIR_NEEDED,
+	SUBPROGRAM_CHAIR_NEEDED,
+	REVIEWS_NEEDED,
+	RECOMMENDATION_NEEDED,
+	DECISION_NEEDED,
+	ACCEPT,
+	DECLINE;
+}
