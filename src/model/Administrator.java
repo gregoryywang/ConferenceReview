fb3551ba3ebbcee843ffd7a81924ab607852de37
dqt@uw.edu <dqt@uw.edu@e314a958-d412-a368-9749-d0e056828d1e>
2013-05-15 03:35:09 +0000

    Filling out basic constructors, setters, getters for model classes.
    Worked on Administrator, Conference, and User.  Administrator is not functioning properly due to a question about what a default Conference would be for such a user when defining the constructor.

--- src/model/Administrator.java
+++ src/model/Administrator.java
@@ -1,16 +1,24 @@
 package model;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
-public class Administrator 
+public class Administrator extends User
 {
-	public Administrator()
+
+	public Administrator(final int the_id, final String the_first_name, final String the_last_name, 
+			final String the_username, final String the_password, final String the_email)
 	{
-		
+		//super(the_id, the_first_name, the_last_name, 
+			//	the_username, the_password, the_email);
+		super.setRole(Role.ADMIN); //not sure if this is a good idea.  What is another way?
 	}
 	
 	public void createConference()
 	{
+		Map<Conference.Deadline, Date> deadlines = new HashMap<Conference.Deadline, Date>();
+		
 		
 	}
 
@@ -18,4 +26,5 @@ public class Administrator
 	{
 		
 	}
+	
 }
--- src/model/User.java
+++ src/model/User.java
@@ -8,13 +8,13 @@ import java.util.Observable;
  * @author yongyuwang
  *
  */
-
 public class User extends Observable
 {
 	private final int my_id;
 	
+	//push this down to childern?
 	private Conference my_conference;
-	
+	//push this down to children?
 	private Role my_role;
 	
 	private String my_first_name;
@@ -27,17 +27,37 @@ public class User extends Observable
 	
 	private String my_email;
 	
-	public User(final int the_id, final String the_first_name, final String the_last_name, 
+	public User(final int the_id, final Conference the_conference, final Role the_role, 
+			final String the_first_name, final String the_last_name, 
 			final String the_username, final String the_password, final String the_email)
 	{
 		my_id = the_id;
+		my_conference = new Conference(the_conference);
+		my_role = the_role;
 		my_first_name = the_first_name;
 		my_last_name = the_last_name;
 		my_username = the_username;
 		my_password = the_password;
 		my_email = the_email;
 	}
+
+	//what would default conference be?????
+	/*
+	public User(final int the_id, final String the_first_name, final String the_last_name, 
+			final String the_username, final String the_password, final String the_email)
+	{
+		this(the_id, new Conference(), Role.USER, the_first_name, the_last_name, 
+				the_username, the_password, the_email);
+	}
+	*/
 	
+	public User(final User the_user)
+	{
+		this(the_user.getID(), the_user.getConference(), the_user.getRole(),
+				the_user.getFirstName(), the_user.getLastName(), 
+				the_user.getUsername(), the_user.getPassword(), the_user.getEmail());		
+	}
+
 	public int getID()
 	{
 		return my_id;
@@ -46,6 +66,8 @@ public class User extends Observable
 	public void setConference(final Conference the_conference)
 	{
 		my_conference = the_conference; //need to make a copy to be safe...
+		setChanged();
+		notifyObservers(the_conference);
 	}
 	
 	public Conference getConference()
@@ -56,6 +78,8 @@ public class User extends Observable
 	public void setRole(final Role the_role)
 	{
 		my_role = the_role;
+		setChanged();
+		notifyObservers(the_role);
 	}
 	
 	public Role getRole()
@@ -66,6 +90,8 @@ public class User extends Observable
 	public void setFirstName(final String the_first_name)
 	{
 		my_first_name = the_first_name;
+		setChanged();
+		notifyObservers(my_first_name);
 	}
 	
 	public String getFirstName()
@@ -76,6 +102,8 @@ public class User extends Observable
 	public void setLastName(final String the_last_name)
 	{
 		my_last_name = the_last_name;
+		setChanged();
+		notifyObservers(my_last_name);
 	}
 	
 	public String getLastName()
@@ -101,6 +129,8 @@ public class User extends Observable
 	public void setEmail(final String the_email)
 	{
 		my_email = the_email;
+		setChanged();
+		notifyObservers(my_email);
 	}
 	
 	public String getEmail()
