3f907f20b96e911491259cf6f1adf9acc9cc4f91
yongwang@uw.edu <yongwang@uw.edu@e314a958-d412-a368-9749-d0e056828d1e>
2013-05-15 22:09:28 +0000

    Resolved merge conflicts.

--- src/GUI/MainView.java
+++ src/GUI/MainView.java
@@ -1,5 +1,52 @@
 package GUI;
 
-public class MainView {
+/**
+ * MainView.java
+ * Creates a JFrame panel from which all other UI contents can be displayed.
+ * @author yongyuwang
+ * @version BETA-5-15-1252
+ */
+
+//TODO: Fix import mess/sort out dependencies.
+import java.awt.BorderLayout;
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+
+public class MainView extends JFrame {
+
+	/**
+	 * Default generated serial code.
+	 */
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainView frame = new MainView();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public MainView() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
 
 }
