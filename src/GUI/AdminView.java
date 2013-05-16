f321882c34abd39c124c6f5a88e983e3a8633e9a
levonk@uw.edu <levonk@uw.edu@e314a958-d412-a368-9749-d0e056828d1e>
2013-05-16 01:19:31 +0000

    TCSS 360 - Software Development and Quality Assurance 15 May, 2013 Team: Da-Deliverables Group Project
    Finished:
    
    -finished creating the barebones ConferenceForm class
    -added comments to the AdminView class
    -added comments to the Administrator class
    
    Things to do:
    
    -need to finished the Administrator class
    -need to finish the AdminView class
    -need to finish the ConferenceForm class

--- src/GUI/AdminView.java
+++ src/GUI/AdminView.java
@@ -1,5 +1,47 @@
 package GUI;
 
-public class AdminView {
+import java.util.Collection;
 
+import model.Paper;
+import model.Review;
+import model.Viewer;
+
+/**
+ * Class to create a AdministratorView Object that
+ * represents what an Administrator User Interface
+ * looks like.
+ * 
+ * @author GuestUser
+ * @version 1.0
+ */
+public class AdminView implements Viewer
+{
+	/**
+	 * Creates a default AdminView Object.
+	 */
+	public AdminView()
+	{
+		
+	}
+	
+	/**
+	 * Returns a Collection of Paper Objects.
+	 * 
+	 * @return the Collection of Paper Objects
+	 */
+	@Override
+	public Collection<Paper> viewPapers() {
+		return null;
+	}
+
+	/**
+	 * Returns a Collection of Review Objects.
+	 * 
+	 * @return the Collection of Review Objects
+	 */
+	@Override
+	public Collection<Review> viewReviews() {
+		return null;
+	}
+	
 }
