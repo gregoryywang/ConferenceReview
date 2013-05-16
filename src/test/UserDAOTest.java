d8b06a00705242b92206890fdc971a837c62bfda
rjonesca97@gmail.com <rjonesca97@gmail.com>
2013-05-16 00:06:48 +0000

    Added Test for UserDAO

---
+++ src/test/UserDAOTest.java
@@ -0,0 +1,53 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import java.util.Collection;
+
+import model.Role;
+import model.User;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import dao.UserDAO;
+
+public class UserDAOTest {
+
+  private UserDAO userDao = null;
+  
+  /**
+   * Test fixture for tests.
+   */
+  @Before
+  public void setUp() {
+    userDao = new UserDAO();
+  }
+  
+  /**
+   * Tests the authentication system with a bad combination.
+   */
+  @Test
+  public void testAuthenticateFail() {
+    boolean result = userDao.authenticate("bob".hashCode(), "barker".hashCode());
+    assertFalse(result);
+  }
+  
+  /**
+   * Tests the authentication system with a good combination.
+   */
+  @Test
+  public void testAuthenticatePass() {
+    boolean result = userDao.authenticate("robert".hashCode(), "barker".hashCode());
+    assertTrue(result);
+  }
+  
+  /**
+   * Tests with roles collection returns not empty with valid input.
+   */
+  @Test
+  public void testGetRoles() {
+    Collection<Role> result = userDao.getRoles("test".hashCode(), "test".hashCode());
+    assertFalse(result.isEmpty());
+  }
+}
