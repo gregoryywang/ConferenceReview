41ff347d3db2e62bb4efb7bc098893bd40ccc324
levonk@uw.edu <levonk@uw.edu@e314a958-d412-a368-9749-d0e056828d1e>
2013-05-20 13:55:34 +0000

    TCSS 360 - Software Development and Quality Assurance 20 May, 2013 Group Project
    Finished:
    
    -finished the Administrator class (general methods and constructors)
    
    To Do:
    
    -need to finalize Administrator class
    - need to finish creating the AdminView layout
    - need to finish creating the ConferenceForm layout

--- src/model/Administrator.java
+++ src/model/Administrator.java
@@ -2,6 +2,10 @@ package model;
 
 import java.util.Date;
 
+import dao.AbstractDAO;
+import dao.ConferenceDAO;
+import dao.UserDAO;
+
 /**
  * Class to create an Administrator Object which
  * has all the functionality of a User plus
@@ -13,6 +17,25 @@ import java.util.Date;
  */
 public class Administrator extends User
 {
+	/**
+	 * Reference to the Conference Data Access Object 
+	 * that manipulates the Conference Object/s in the
+	 * Database.
+	 */
+	private ConferenceDAO my_conference_dao;
+	
+	/*
+	/**
+	 * Reference to the User Data Access Object
+	 * that manipulates the Database based on
+	 * the current users Role (i.e. Role.ADMIN)
+	 */
+	 //	private UserDAO my_user_dao;
+	 /*
+	 * NOT SURE IF I WILL NEED THIS GUY
+	 */
+	
+	
 	/**
 	 * Constructs an Administrator object from the
 	 * given parameters.
@@ -30,6 +53,13 @@ public class Administrator extends User
 	{
 		super(null, Role.ADMIN, the_first_name, the_last_name, 
 				the_username, the_password, the_email);
+		my_conference_dao = new ConferenceDAO();
+		
+		/*
+		 * Not sure if I will need this guys
+		 */ 
+		 // my_user_dao = new UserDAO();
+		 
 	}
 	
 	/**
@@ -41,7 +71,7 @@ public class Administrator extends User
 	 */
 	public void createConference(final Conference the_conference)
 	{	
-		
+		my_conference_dao.addConference(the_conference);
 	}
 
 	/**
@@ -51,7 +81,20 @@ public class Administrator extends User
 	 */
 	public void changeDate(final Date the_date)
 	{
+		/*
+		 * depends how I know which conference I am manipulating
+		 * I am assuming it is by ID but not sure how to
+		 * obtain the correct conference to change its Date Object. 
+		 */
+		final int stub_id = 0;
 		
+		my_conference_dao.getConferences()
+		  .get(stub_id).setDate(the_date);
+	}
+	
+	public AbstractDAO getConferenceDAO()
+	{
+		return my_conference_dao;
 	}
 	
 }
