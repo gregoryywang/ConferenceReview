9f66963916b44893822d8693d0ac0124dc20eaf0
rjonesca97@gmail.com <rjonesca97@gmail.com@e314a958-d412-a368-9749-d0e056828d1e>
2013-05-21 05:36:55 +0000

    Added Conference table

--- src/sql/tables.sql
+++ src/sql/tables.sql
@@ -8,12 +8,13 @@ password varchar(15),email_address varchar(50),
 Primary Key(user_id))
 
 --Create Conference Table
+create table conference(conf_id int not null, topic varchar(20) not null, conference_date Timestamp not null, author_sub_deadline Timestamp not null, reviewer_sub_deadline Timestamp not null, author_notification_deadline Timestamp not null, primary key(conf_id));
 
 --Create Role Type Table
 create table role_type(role_id int not null auto_increment, title varchar(15) not null, role_type int not null);
 
 --Create UserRolePaperConference Table
-create table user_role_paper_conference_join(user_id int not null, role_id int not null, paper_id int, conf_id int not null, primary key(user_id, role_id, conf_id), foreign key(user_id) references user, foreign key(role_id) references role_type, foreign key(paper_id) references paper, foreign key(conf_id) references conference);
+create table user_role_paper_conference_join(user_id int not null, role_id int not null, paper_id int, conf_id int, primary key(user_id, role_id, conf_id), foreign key(user_id) references user, foreign key(role_id) references role_type, foreign key(paper_id) references paper, foreign key(conf_id) references conference);
 
 --Populate with data
 --Add Admin user
