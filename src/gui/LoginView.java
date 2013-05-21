1f541581ffd750779077e262aa17a30af3bff131
Yong Yu Wang <yongyuwang@users.noreply.github.com>
2013-05-21 01:57:56 +0000

    Test to see if LoginView closes.

--- src/gui/LoginView.java
+++ src/gui/LoginView.java
@@ -3,9 +3,10 @@ package gui;
 /**
  * LoginView.java
  * @author yongyuwang
- * @version 5-15-0654
- * Demonstrates the utilization of inner classes to combine JFrame and
- * ActionListener
+ * @version 5-20-1817
+ * Displays Login window, authenticates user credentials and gets 
+ * authenticated user.
+ * Implements JDialog modal behavior.
  */
 
 import java.awt.*;
@@ -72,94 +73,20 @@ public class LoginView extends JDialog implements ActionListener{
 		user = user_dao.authenticate(usernameField.getText(), password);
 				
 		// returns user if login succeeds 
-		if (user != null) {
-			// DEBUG
-			JOptionPane.showMessageDialog(this, "Success!\nPlease delete at some point!");
-		}
-		else {
+		if (user == null) {
 			// Dialog box pops up when authentication fails. 
 			JOptionPane.showMessageDialog(this, "Sorry, wrong username and/or password.\n" +
-					"Please try again.");
-		}			
+				"Please try again.");
+		}
+		else {
+	        this.setVisible(false);
+	        return;
+		}
 	}
 	
 	User getUser() {
 		return user;
 	}
 }
-
-/**
-
-class LoginView extends JDialog implements ActionListener
-{
-	
-	private static final long serialVersionUID = 1L;
-	
-	final JButton submitButton;
-	final JPanel panel;
-	final JLabel usernameLabel, passwordLabel;
-	final JTextField  usernameField;
-	final JPasswordField passwordField;
-	
-	private User user = null;
-
-	LoginView()
-	{
-		usernameLabel = new JLabel("Username:");
-		usernameField = new JTextField(15);
-
-		passwordLabel = new JLabel("Password:");
-		passwordField = new JPasswordField(15);
-
-		submitButton = new JButton("SUBMIT");
-		submitButton.addActionListener(this);
-
-		panel = new JPanel(new GridLayout(3,2));
-		panel.add(usernameLabel);
-		panel.add(usernameField);
-		panel.add(passwordLabel);
-		panel.add(passwordField);
-		panel.add(submitButton);
-
-		panel.add(new JLabel(""));
-		add(panel,BorderLayout.CENTER);
-
-		setTitle("PLEASE LOGIN");
-		
-		JDialog dialog = new JDialog();
-		dialog.setContentPane(panel);
-		dialog.setSize(300,100);
-	}
-	
-	public void actionPerformed(ActionEvent event)
-	{
-		// creates a UserDAO to authenticate user
-		UserDAO user_dao = new UserDAO(); 
-		
-		// getPassword returns an array of char[], need to convert to String
-		final char[] tempPassword = passwordField.getPassword();
-		final String password = new String(tempPassword);
-		
-		// authenticates user, gets user object
-		user = user_dao.authenticate(usernameField.getText(), password);
-				
-		// returns user if login succeeds 
-		if (user != null) {
-			// DEBUG
-			JOptionPane.showMessageDialog(this, "Success!\nPlease delete at some point!");
-		}
-		else {
-			// Dialog box pops up when authentication fails. 
-			JOptionPane.showMessageDialog(this, "Sorry, wrong username and/or password.\n" +
-					"Please try again.");
-		}			
-	}
-	
-	User getUser() {
-		return user;
-	}	
-}
-
-**/
 
 
