93eec663713d7171d216980a3b2172dd1e596f47
rjonesca97@gmail.com <rjonesca97@gmail.com>
2013-05-21 05:36:03 +0000

    changed method signature.

--- src/model/Administrator.java
+++ src/model/Administrator.java
@@ -2,10 +2,6 @@ package model;
 
 import java.util.Date;
 
-import dao.AbstractDAO;
-import dao.ConferenceDAO;
-import dao.UserDAO;
-
 /**
  * Class to create an Administrator Object which
  * has all the functionality of a User plus
@@ -17,25 +13,6 @@ import dao.UserDAO;
  */
 public class Administrator extends User
 {
-	/**
-	 * Reference to the Conference Data Access Object 
-	 * that manipulates the Conference Object/s in the
-	 * Database.
-	 */
-	private ConferenceDAO my_conference_dao;
-	
-	/*
-	/**
-	 * Reference to the User Data Access Object
-	 * that manipulates the Database based on
-	 * the current users Role (i.e. Role.ADMIN)
-	 */
-	 //	private UserDAO my_user_dao;
-	 /*
-	 * NOT SURE IF I WILL NEED THIS GUY
-	 */
-	
-	
 	/**
 	 * Constructs an Administrator object from the
 	 * given parameters.
@@ -53,13 +30,6 @@ public class Administrator extends User
 	{
 		super(null, Role.ADMIN, the_first_name, the_last_name, 
 				the_username, the_password, the_email);
-		my_conference_dao = new ConferenceDAO();
-		
-		/*
-		 * Not sure if I will need this guys
-		 */ 
-		 // my_user_dao = new UserDAO();
-		 
 	}
 	
 	/**
@@ -71,7 +41,7 @@ public class Administrator extends User
 	 */
 	public void createConference(final Conference the_conference)
 	{	
-		my_conference_dao.addConference(the_conference);
+		
 	}
 
 	/**
@@ -81,20 +51,7 @@ public class Administrator extends User
 	 */
 	public void changeDate(final Date the_date)
 	{
-		/*
-		 * depends how I know which conference I am manipulating
-		 * I am assuming it is by ID but not sure how to
-		 * obtain the correct conference to change its Date Object. 
-		 */
-		final int stub_id = 0;
 		
-		my_conference_dao.getConferences()
-		  .get(stub_id).setDate(the_date);
-	}
-	
-	public AbstractDAO getConferenceDAO()
-	{
-		return my_conference_dao;
 	}
 	
 }
