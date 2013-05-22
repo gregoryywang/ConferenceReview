7b5a0275bcc12933c607306d387a8b777337f7fc
rjonesca97@gmail.com <rjonesca97@gmail.com@e314a958-d412-a368-9749-d0e056828d1e>
2013-05-22 03:48:38 +0000

    Added additional testing code.

--- src/test/ConferenceDAOTest.java
+++ src/test/ConferenceDAOTest.java
@@ -2,6 +2,11 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.sql.Date;
+
+import model.Conference;
+import model.Conference.Deadline;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -27,8 +32,37 @@ public class ConferenceDAOTest {
    * Tests adding a conference.
    */
   @Test
-  public void testAddConference() {
-    fail();
+  public void testSaveConference() {
+    Conference conference = new Conference();
+    conference.setTopic("Test Topic");
+    conference.setDate(new Date(0));
+    conference.setDeadline(Deadline.SUBMIT_PAPER, new Date(0));
+    conference.setDeadline(Deadline.REVISE_PAPER, new Date(0));
+    conference.setDeadline(Deadline.REVIEW_PAPER, new Date(0));
+    conference.setDeadline(Deadline.MAKE_RECOMMENDATION, new Date(0));
+    conference.setDeadline(Deadline.FINAL_DECISION, new Date(0));
+    
+    //Save conference
+    conferenceDao.saveConference(conference);
+  }
+  
+  /**
+   * Tests updating an existing conference.
+   */
+  @Test
+  public void testSaveConferenceUpdate() {
+    Conference conference = new Conference();
+    conference.setTopic("Updated Test Topic");
+    conference.setID(2);
+    conference.setDate(new Date(0));
+    conference.setDeadline(Deadline.SUBMIT_PAPER, new Date(0));
+    conference.setDeadline(Deadline.REVISE_PAPER, new Date(0));
+    conference.setDeadline(Deadline.REVIEW_PAPER, new Date(0));
+    conference.setDeadline(Deadline.MAKE_RECOMMENDATION, new Date(0));
+    conference.setDeadline(Deadline.FINAL_DECISION, new Date(0));
+    
+    //Save conference
+    conferenceDao.saveConference(conference);
   }
   
   /**
