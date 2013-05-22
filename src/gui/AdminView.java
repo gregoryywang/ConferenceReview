e24d5e492f88ee58691d6f07d5b94408ae2c40f2
levonk@uw.edu <levonk@uw.edu>
2013-05-22 23:05:14 +0000

    TCSS 360 Software Development and Quality Assurance 22 May, 2013 Group Project
    Finished:
    
    - finished a rough outline of the ConferenceForm class
    - finished a rough outline of the Administrator class
    - finished a rough outline of the AdminView class
    
    To Do:
    
    - need to pull the data form the ConferenceForm fields and create a Conference Object
    - need to save the Conference Object to the Database
    - need to figure out a better way for the flow of data in the Administrator class
    - need to figure out a better way for the flow of data in the AdminView class

--- src/gui/AdminView.java
+++ src/gui/AdminView.java
@@ -1,17 +1,31 @@
 package gui;
 
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 import model.Administrator;
 import model.Paper;
 import model.Review;
+import model.User;
 import model.Viewer;
+
+import common.ReferenceObject;
+
 import dao.ConferenceDAO;
+import dao.UserDAO;
 
 /**
  * Class to create a AdministratorView Object that
@@ -31,6 +45,16 @@ public class AdminView extends JPanel implements Viewer
 	private static final int STUB_ID = 0;
 	
 	/**
+	 * The default number of panel rows.
+	 */
+	private static final int PANEL_ROWS = 0;
+	
+	/**
+	 * The defualt number of panel columns.
+	 */
+	private static final int PANEL_COLUMNS = 3;
+	
+	/**
 	 * The default serial version UID.
 	 */
 	private static final long serialVersionUID = 1L;
@@ -42,13 +66,34 @@ public class AdminView extends JPanel implements Viewer
 	private Administrator my_administrator;
 	
 	/**
+	 * Reference to the ConferenceDAO Object
+	 * used to manipulate the database data.
+	 */
+	private ConferenceDAO my_conference_dao;
+	
+	/**
+	 * Reference to the ReferenceObject Object
+	 * used to manipulate the database data.
+	 */
+	private ReferenceObject my_reference_dao;
+	
+	/**
+	 * Reference to the UserDAO Object
+	 * used to manipulate the database data.
+	 */
+	private UserDAO my_user_dao;
+	
+	/**
 	 * Creates a default AdminView Object.
 	 */
 	public AdminView()
 	{
-		super();
-		my_administrator = new Administrator(0, "Admin", 
-			"istrator", "SU", "root", "barackobama@thepresi.dent");
+		super(new BorderLayout());
+		my_administrator = new Administrator(0, "admin", 
+			"admin", "Super User", "root", "barackobama@thepresi.dent");
+		my_conference_dao = new ConferenceDAO();
+		my_reference_dao = new ReferenceObject();
+		my_user_dao = new UserDAO();
 	}
 	
 	/**
@@ -56,21 +101,158 @@ public class AdminView extends JPanel implements Viewer
 	 */
 	public void fillPanel()
 	{
-		final JPanel panel = new JPanel(new GridLayout(2, 2));
+		add(createNorthernPanel(), BorderLayout.NORTH);
+		add(createWesternPanel(), BorderLayout.WEST);
+		add(createSouthernPanel(), BorderLayout.SOUTH);
+		add(createCenterPanel(), BorderLayout.CENTER);
+	}
+	
+	private JPanel createNorthernPanel()
+	{
+		final JPanel northern_panel = new JPanel(new FlowLayout());
 		
-		final JLabel papers = new JLabel("Papers");
-		//final JLabel paper_list = new JLabel(viewPapers().toArray().toString());
-		final JLabel paper_list = new JLabel("THE PAPER LIST");
-		final JLabel reviews = new JLabel("Reviews");
-		//final JLabel review_list = new JLabel(viewReviews().toArray().toString());
-		final JLabel review_list = new JLabel("THE REVIEW LIST");
+		final JLabel username_label = new JLabel("Hello, Administrator");
+		northern_panel.add(username_label);
 		
-		panel.add(papers);
-		panel.add(paper_list);
-		panel.add(reviews);
-		panel.add(review_list);
+		return northern_panel;
+	}
+	
+	private JPanel createWesternPanel()
+	{
+		final JPanel western_panel = new JPanel(new GridLayout(0, 1));
 		
-		add(panel);
+		final JButton signout_button = new JButton("Sign Out");
+		signout_button.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(final ActionEvent the_event)
+			{
+				int choice = JOptionPane.showConfirmDialog(null, "Are you sure you want to sign out?");
+				if (choice == JOptionPane.YES_OPTION)
+					{
+						JOptionPane.showMessageDialog(null, "Nice try!");
+					}
+			}
+		});
+		western_panel.add(signout_button);
+		
+		final JButton change_password_button = new JButton("Change Password");
+		change_password_button.addActionListener(new ActionListener()
+			{
+				public void actionPerformed(final ActionEvent the_event)
+				{
+					JOptionPane.showMessageDialog(null, 
+						"Your password has been set to \"password123\"");
+				}
+			});
+		western_panel.add(change_password_button);
+		
+		final JButton change_email_button = new JButton("Change E-Mail");
+		change_email_button.addActionListener(new ActionListener()
+			{
+				public void actionPerformed(final ActionEvent the_event)
+				{
+					JOptionPane.showMessageDialog(null,
+						"Expect to hear from online advertisers =-o");
+				}
+			});
+		western_panel.add(change_email_button);
+		
+		final JButton etc_button = new JButton("etc..");
+		etc_button.addActionListener(new ActionListener()
+			{
+				public void actionPerformed(final ActionEvent the_event)
+				{
+					JOptionPane.showMessageDialog(null, "Hey!!! Don't Click that!!!!");
+				}
+			});
+		western_panel.add(etc_button);
+		
+		return western_panel;
+	}
+	
+	private JPanel createSouthernPanel()
+	{
+		final JPanel southern_panel = new JPanel(new FlowLayout());
+		
+		southern_panel.add(new JButton("Why am I here?"));
+		
+		return southern_panel;
+	}
+	
+	private JPanel createCenterPanel()
+	{
+		final JPanel center_panel = new JPanel();
+		
+		center_panel.add(new JLabel("\t Conferences "));
+		final JComboBox conference_combo_box = new JComboBox(
+			my_conference_dao.getConferencesRef().toArray());
+		conference_combo_box.addItem("NEW CONFERENCE");
+		center_panel.add(conference_combo_box);
+		final JButton button = new JButton("Go");
+		button.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(final ActionEvent the_event)
+			{
+				createConferenceForm();
+			}
+		});
+		center_panel.add(button);
+		
+		return center_panel;
+	}
+	
+	
+	/**
+	 * Method to create a ConferenceForm Object
+	 * based on what conference they chose
+	 * in the JComboBox.
+	 */
+	public void createConferenceForm()
+	{
+		new ConferenceForm(this).start();
+	}
+	
+	/**
+	 * Method to get the Administrator Object associated
+	 * with this AdminView.
+	 * 
+	 * @return returns the Administrator Object
+	 */
+	public User getAdmin()
+	{
+		return my_administrator;
+	}
+	
+	/**
+	 * Method to get the ConferenceDAO Object associated
+	 * with this AdminView.
+	 * 
+	 * @return returns the ConferenceDAO Object
+	 */
+	public ConferenceDAO getConferenceDAO()
+	{
+		return my_conference_dao;
+	}
+	
+	/**
+	 * Method to get the ConferenceDAO Object associated
+	 * with this AdminView.
+	 * 
+	 * @return returns the ConferenceDAO Object
+	 */
+	public UserDAO getUserDAO()
+	{
+		return my_user_dao;
+	}
+	
+	/**
+	 * Method to get the ReferenceDAO Object associated
+	 * with this AdminView
+	 * @return
+	 */
+	public ReferenceObject getReferenceDAO()
+	{
+		return my_reference_dao;
 	}
 	
 	/**
@@ -82,14 +264,14 @@ public class AdminView extends JPanel implements Viewer
 	public List<Paper> viewPapers() 
 	{
 	  return new ArrayList<Paper>();
-		//return new ArrayList<Paper>(((ConferenceDAO) my_administrator.getConferenceDAO())
-		//	.getConferencesRef().get(STUB_ID).getPapers());
+	//	return new ArrayList<Paper>(((ConferenceDAO) my_administrator.getConferenceDAO())
+	//		.getConferencesRef().get(STUB_ID).getPapers());
 	}
 
 	/**
-	 * Returns a Collection of Review Objects.
+	 * Returns a List of Review Objects.
 	 * 
-	 * @return the Collection of Review Objects
+	 * @return the List of Review Objects
 	 */
 	@Override
 	public List<Review> viewReviews() 
@@ -97,6 +279,17 @@ public class AdminView extends JPanel implements Viewer
 	  return new ArrayList<Review>();
 	  //return new ArrayList<Review>(((ConferenceDAO)my_administrator.getConferenceDAO())
 		//	.getConferences().get(STUB_ID).getPapers().get(STUB_ID).getReviews());
+	}
+	
+	public static void main(String[] args)
+	{
+		final JFrame frame = new JFrame("AdminView");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		final AdminView admin_view = new AdminView();
+		admin_view.fillPanel();
+		frame.add(admin_view);
+		frame.pack();
+		frame.setVisible(true);
 	}
 	
 }
--- src/gui/ConferenceForm.java
+++ src/gui/ConferenceForm.java
@@ -4,10 +4,18 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import model.Viewer;
 
 /**
  * Class that creates a ConferenceForm Object
@@ -35,34 +43,61 @@ public class ConferenceForm extends JFrame
 	private static final int FRAME_HEIGHT = 300;
 	
 	/**
+	 * The defualt background color.
+	 */
+	private static final Color BACKGROUND_COLOR = Color.GREEN;
+	
+	/**
+	 * Reference to the current users Viewer Object.
+	 */
+	private Viewer my_view;
+	
+	/**
+	 * Reference to whether the current Viewer is an or not.
+	 */
+	private boolean my_admin_flag = false;
+	
+	/**
 	 * Creates a default ConferenceForm Object.
 	 */
-	public ConferenceForm()
+	public ConferenceForm(final Viewer the_view)
 	{
 		super("Conference Form");
-		setPreferredSize(new Dimension(FRAME_WIDTH, FRAME_HEIGHT));
+		setSize(new Dimension(FRAME_WIDTH, FRAME_HEIGHT));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		my_view = the_view;
+		if ("AdminView".equals(my_view.getClass().getSimpleName()))
+		{
+			my_admin_flag = true;
+		}
 	}
 	
 	public void start()
 	{
 		add(new ConferencePanel(), BorderLayout.CENTER);
-		add(new JLabel("THIS IS THE NORTH LABEL"), BorderLayout.NORTH);
-		add(new JLabel("This is the DIRTY SOUF label!!"), BorderLayout.SOUTH);
-		add(new JLabel("This is the WEST COAST labeeel"), BorderLayout.WEST);
-		add(new JLabel("This is the EASY COASTY LBl"), BorderLayout.EAST);
+		
+		final JButton button = new JButton("Create / Save");
+		button.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(final ActionEvent the_event)
+			{
+				JOptionPane.showMessageDialog(null, 
+					"You have succesfully created / saved the conference!");
+				dispose();
+			}
+		});
+		final JPanel button_panel = new JPanel();
+		button_panel.setBackground(BACKGROUND_COLOR);
+		button_panel.add(button);
+		add(button_panel, BorderLayout.SOUTH);
+		
 		setVisible(true);
 		pack();
 	}
 	
-	/**
-	 * Method to test my GUI.
-	 * 
-	 * @param the_args the command-line arguments
-	 */
-	public static void main(final String[] the_args)
+	public static void main(String[] args)
 	{
-		new ConferenceForm().start();
+		new ConferenceForm(new AdminView()).start();
 	}
 	
 	private class ConferencePanel extends JPanel
@@ -75,12 +110,28 @@ public class ConferenceForm extends JFrame
 		/**
 		 * The number of textfield rows.
 		 */
-		private static final int ROWS = 2;
+		private static final int ROWS = 0;
 		
 		/**
 		 * The number of textfield columns.
 		 */
-		private static final int COLUMNS = 10;
+		private static final int COLUMNS = 2;
+		
+		/**
+		 * A random integer array.
+		 */
+		private final Integer[] array = {1,2,3,4,5,6,7,8,9,1,0,3,56,43,43};
+		
+		/**
+		 * A random name array.
+		 */
+		private final String[] names = {"joe", "moe", "slow", "pro", "flow", "agro", "viagro"};
+		
+		/**
+		 * A random category array.
+		 */
+		private final String[] categories = {"categories shmatergories", "scattergories", "cats", "dogs", 
+			"frogs", "logs", "bogs", "hogs"};
 
 		/**
 		 * Constructs a default ConferencePanel Object.
@@ -88,10 +139,65 @@ public class ConferenceForm extends JFrame
 		public ConferencePanel()
 		{
 			super(new GridLayout(ROWS, COLUMNS));
-			setBackground(Color.GREEN);
+			setBackground(BACKGROUND_COLOR);
+			addFields();
+			pack();
 			setVisible(true);
 		}
 		
+		private void addFields()
+		{
+			add(new JLabel("\t Conference Topic: "));
+			// Need to check whether or not to populate the ConferenceForm
+			// with data based on whether or not the Conference is pre-existing
+			final JTextField topic_field = new JTextField(20);
+			topic_field.setEditable(my_admin_flag);
+			add(topic_field);
+			
+			add(new JLabel("\t Conference Date (MM/DD/YY): "));
+			final JTextField date_field = new JTextField(20);
+			date_field.setEditable(my_admin_flag);
+			add(date_field);
+			
+			add(new JLabel("\t Program Chair: "));
+		//	pg_chair_panel.add(new JComboBox(((AdminView) my_view).getUserDAO().getUsersRef().toArray()));
+			add(new JComboBox(names));
+			
+			add(new JLabel("\t Submission Deadline (MM/DD/YYYY): "));
+			final JTextField submission_deadline_field = new JTextField(10);
+			submission_deadline_field.setEditable(my_admin_flag);
+			add(submission_deadline_field);
+
+			add(new JLabel("\t Review Deadline (MM/DD/YYYY): "));
+			final JTextField review_deadline_field = new JTextField(10);
+			review_deadline_field.setEditable(my_admin_flag);
+			add(review_deadline_field);
+			
+			add(new JLabel("\t Recommendation Deadline (MM/DD/YYYY): "));
+			final JTextField recommendation_deadline_field = new JTextField(10);
+			recommendation_deadline_field.setEditable(my_admin_flag);
+			add(recommendation_deadline_field);
+			
+			add(new JLabel("\t Final Decision Deadline (MM/DD/YYYY): "));
+			final JTextField final_decision_field = new JTextField(10);
+			final_decision_field.setEditable(my_admin_flag);
+			add(final_decision_field);
+			
+			add(new JLabel("\t Revision Deadline (MM/DD/YYYY): "));
+			final JTextField revision_field = new JTextField(10);
+			revision_field.setEditable(my_admin_flag);
+			add(revision_field);
+			
+			add(new JLabel("\t Notification Deadline (MM/DD/YYYY): "));
+			final JTextField notification_field = new JTextField(10);
+			notification_field.setEditable(false);
+			add(notification_field);
+			
+			add(new JLabel("\t Conference Categories:"));
+			final JComboBox conference_categories = new JComboBox(categories);
+			add(conference_categories);
+			
+		}
 		
 	}
 	
