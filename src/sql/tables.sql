3a56e3d29faa068f954b12a9dee70d31fdf3abf7
Danielle Tucker <danielle@tuckerlabs.com>
2013-05-23 00:07:45 +0000

    Added needed columns to Conference Table for the various deadlines which were overlooked.
    Added Subprogram Chair deadline and paper revision deadline.

--- src/sql/tables.sql
+++ src/sql/tables.sql
@@ -8,17 +8,37 @@ password varchar(15),email_address varchar(50),
 Primary Key(user_id))
 
 --Create Conference Table
-create table conference(conf_id int not null auto_increment, topic varchar(20) not null, conference_date Timestamp not null, author_sub_deadline Timestamp not null, reviewer_sub_deadline Timestamp not null, author_notification_deadline Timestamp not null, primary key(conf_id));
+create table conference(
+conf_id int not null, 
+topic varchar(20) not null, 
+conference_date Timestamp not null, 
+author_sub_deadline Timestamp not null, 
+reviewer_sub_deadline Timestamp not null,
+sub_prgm_chair_sub_deadline Timestamp not null,
+author_notification_deadline Timestamp not null,
+revision_deadline Timestamp not null, 
+primary key(conf_id));
 
 --Create Role Type Table
-create table role_type(role_id int not null auto_increment, title varchar(15) not null, role_type int not null);
+create table role_type(role_id int not null auto_increment, title varchar(16) not null, role_type int not null);
 
 --Create UserRolePaperConference Table
-create table user_role_paper_conference_join(user_id int not null, role_id int not null, paper_id int, conf_id int, primary key(user_id, role_id, conf_id), foreign key(user_id) references user, foreign key(role_id) references role_type, foreign key(paper_id) references paper, foreign key(conf_id) references conference);
+create table user_role_paper_conference_join(
+user_id int not null, 
+role_id int not null, 
+paper_id int, 
+conf_id int, 
+primary key(user_id, role_id, conf_id), 
+foreign key(user_id) references user, 
+foreign key(role_id) references role_type, 
+foreign key(paper_id) references paper, 
+foreign key(conf_id) references conference);
 
 --Populate with data
 --Add Admin user
 insert into user(first_name, last_name, username, password, email_address) values('AdminTest', 'AdminTest', 'AdminTest', 'AdminTest','AdminTest@uw.edu');
+--Add general user
+insert into user(first_name, last_name, username, password, email_address) values('Default', 'User', 'UserTest', 'UserTest', 'UserTest@uw.edu');
 
 --Author: Danielle
 --Create Roles
@@ -57,16 +77,6 @@ FOREIGN KEY(conf_id) REFERENCES conference,
 FOREIGN KEY(cat_id) REFERENCES category
 );
 
--- Create Recommendation Table
-CREATE TABLE RECOMMENDATION(
-recomm_id int not null auto_increment,
-user_id int not null,
-rating int not null,
-comments varchar(1000),
-active int,
-PRIMARY KEY(recomm_id),
-FOREIGN KEY(user_id) REFERENCES user,
-);
 
 -- Create Paper Table
 CREATE TABLE PAPER(
@@ -78,13 +88,13 @@ abstract varchar(1000) not null,
 cat_id int not null,
 document_path varchar(50) not null,
 revised_document_path varchar(50),
-recomm_id int,
 status varchar(36),
+recomm_rating int,
+recomm_comments varchar(1000),
 active int,
 PRIMARY KEY(paper_id),
 FOREIGN KEY(user_id) REFERENCES user,
 FOREIGN KEY(cat_id) REFERENCES category,
-FOREIGN KEY(recomm_id) REFERENCES recommendation,
 FOREIGN KEY(cat_id) REFERENCES category,
 );
 
