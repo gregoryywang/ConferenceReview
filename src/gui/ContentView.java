e28e07c10b42d979c5bab4b82fb691e0ab21a714
Yong Yu Wang <yongyuwang@users.noreply.github.com>
2013-05-23 00:14:34 +0000

    Started work on ContentView, objective is to successfully display AdminView.

--- src/gui/ContentView.java
+++ src/gui/ContentView.java
@@ -1,5 +1,29 @@
 package gui;
 
-public class ContentView {
+/**
+ * ContentView.java
+ * @author yongyuwang
+ * @version 5-22-1618
+ * 
+ * Stub for ContentView. Details under construction.
+ */
+
+import javax.swing.JPanel;
+
+import model.Role;
+import model.User;
+
+public class ContentView extends JPanel {
+	
+	public ContentView(User the_user) {
+		Role the_role = the_user.getRole();
+		if (the_role.equals("Administrator")) {
+			
+			
+		}
+		
+		
+	}
+	
 
 }
