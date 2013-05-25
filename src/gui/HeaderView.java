6da4c493460adb1f19cfafb97f4375a901ca4455
dqt@uw.edu <dqt@uw.edu@e314a958-d412-a368-9749-d0e056828d1e>
2013-05-25 15:21:03 +0000

    Go button working.  Need to update user's conference on go but need ConferenceService method created first.

--- src/gui/HeaderView.java
+++ src/gui/HeaderView.java
@@ -38,7 +38,7 @@ public class HeaderView extends JPanel
 	private final JComboBox conference_selector = new JComboBox();
 	private User my_user;
 	private boolean is_admin = false;
-	
+
 	/**
 	 * Create the panel.
 	 */
@@ -56,12 +56,12 @@ public class HeaderView extends JPanel
 			my_user.setLastName("");
 		}
 		setLayout(new BorderLayout(0, 0));
-		
+
 		JPanel panel_selection = new JPanel();
 		FlowLayout flowLayout = (FlowLayout) panel_selection.getLayout();
 		flowLayout.setAlignment(FlowLayout.LEFT);
 		add(panel_selection, BorderLayout.SOUTH);
-		
+
 		panel_selection.add(new JLabel("Conference"));
 		makeConferenceSelector();
 		panel_selection.add(conference_selector);
@@ -71,19 +71,42 @@ public class HeaderView extends JPanel
 		role_selector.setEnabled(false);
 		role_selector.addItem(new ReferenceObject("none", "    "));
 		panel_selection.add(role_selector);
-		
-		JButton button_go = new JButton("Go");
+
+		JButton button_go = makeGoButton();
 		panel_selection.add(button_go);
-		
-		
+
+
 		//EAST "Welcome Full Name"
 		JPanel panel_1 = new JPanel();
 		add(panel_1, BorderLayout.EAST);
 		panel_1.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
-			
+
 		panel_1.add(new JLabel("Welcome " + my_user.getFullName()));
-		
+
 		//add logout button???
+	}
+
+	private JButton makeGoButton() {
+		JButton button_go = new JButton("Go");
+		button_go.addActionListener(new ActionListener()
+		{
+			@Override
+			public void actionPerformed(final ActionEvent the_event) 
+			{
+				ReferenceObject role_ro = (ReferenceObject) role_selector.getSelectedItem();
+				ReferenceObject conf_ro = (ReferenceObject) conference_selector.getSelectedItem();
+				//check to see if user hit button without making selection before proceeding.
+				if(role_ro.getData() != null && (Integer) conf_ro.getData() != 0)
+				{
+					//FIX ME WITH SERVICE MODEL THINGINE
+					//Conference conf = ConferenceService.getInstance().getConference((Integer)conf_ro.getData());
+					//my_user.setConference(conf);
+					my_user.setRole((Role)role_ro.getData());
+				}
+			}
+			
+		});
+		return button_go;
 	}
 
 	private void makeConferenceSelector() {	
@@ -98,13 +121,13 @@ public class HeaderView extends JPanel
 		c2.setID(90);
 		ro.add(new ReferenceObject(c1.getTopic(), c1));
 		ro.add(new ReferenceObject(c2.getTopic(), c2));	
-		*/
+		 */
 		//end of testing
-		
+
 		final List<ReferenceObject> ro = ConferenceService.getInstance().getConferences();
 		final ReferenceObject instruct = new ReferenceObject("--select a conference--", 0);
 		ro.add(0, instruct);
-		
+
 		if(is_admin) 
 		{
 			ro.add(0, new ReferenceObject("CREATE NEW CONFERENCE", 0));
@@ -119,37 +142,28 @@ public class HeaderView extends JPanel
 				ReferenceObject conference_name = (ReferenceObject) jcb.getSelectedItem();
 				int conf_id_selected = (Integer) conference_name.getData();
 				System.out.println(conf_id_selected);
-				
+
 				//Remove the instructions "--select a conference--"
 				ro.remove(instruct);
 				conference_selector.setModel(new DefaultComboBoxModel(ro.toArray()));
 				conference_selector.setSelectedItem(conference_name);
-				
-				
+
+
 				role_selector.setEnabled(true);
-				List<ReferenceObject> ro_roles;
-				if(is_admin)
+				List<ReferenceObject> ro_roles = UserService.getInstance().getRoles(my_user.getID(), conf_id_selected);
+				//Test if the author role is included in the result set, if not, add to result set.
+				boolean has_author = false;
+				for(ReferenceObject roles: ro_roles)
 				{
-					ro_roles = new ArrayList<ReferenceObject>();
-					ro_roles.add(new ReferenceObject(Role.ADMIN.toString(), Role.ADMIN));
-				}
-				else
-				{
-					ro_roles = UserService.getInstance().getRoles(my_user.getID(), conf_id_selected);
-					//Test if the author role is included in the result set, if not, add to result set.
-					boolean has_author = false;
-					for(ReferenceObject roles: ro_roles)
+					if((Role)roles.getData()==Role.AUTHOR)
 					{
-						if((Role)roles.getData()==Role.AUTHOR)
-						{
-							has_author = true;
-						}
+						has_author = true;
 					}
-					if (!has_author)
-					{
-						ro_roles.add(new ReferenceObject(Role.AUTHOR.toString(), Role.AUTHOR));
-					}									
 				}
+				if (!has_author)
+				{
+					ro_roles.add(new ReferenceObject(Role.AUTHOR.toString(), Role.AUTHOR));
+				}									
 				ComboBoxModel role_model = new DefaultComboBoxModel(ro_roles.toArray());
 				role_selector.setModel(role_model);
 			}
@@ -169,7 +183,7 @@ public class HeaderView extends JPanel
 		User user = new User(conf, Role.USER,"Test", "User", "testuser", "testpsswd", "test@notvalid.com");
 		HeaderView hv = new HeaderView(user);
 		hv.setVisible(true);
-		
+
 		JFrame frame = new JFrame();
 		frame.add(hv);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
