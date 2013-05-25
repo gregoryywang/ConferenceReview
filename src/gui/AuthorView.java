0f64ff062df11a9f2991c9f2190c2b26f4b0df3e
yongyuwang5@gmail.com <yongyuwang5@gmail.com@e314a958-d412-a368-9749-d0e056828d1e>
2013-05-25 22:36:35 +0000

    Added experimental table code in AuthorView. Please check today's status email for more info about this.

--- src/gui/AuthorView.java
+++ src/gui/AuthorView.java
@@ -1,5 +1,124 @@
 package gui;
 
-public class AuthorView {
+/**
+ * AuthorView.java
+ * @author yongyuwang
+ * @version 5-25-1424
+ * Prototype table to display author's papers
+ */
 
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.DefaultCellEditor;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.UIManager;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellRenderer;
+
+public class AuthorView extends JFrame {
+
+  public AuthorView() {
+    super("Author View");
+
+    DefaultTableModel AuthorPaperTable = new DefaultTableModel();
+    AuthorPaperTable.setDataVector(new Object[][] {{ "Paper 1 Name", "Paper 1 Status", "Edit" }, { "Paper 2 Name", "Paper 2 Status", "Edit" }}, 
+    		new Object[] { "Paper Name", "Current Status", "Edit Submission" });
+
+    JTable table = new JTable(AuthorPaperTable);
+    table.getColumn("Edit Submission").setCellRenderer(new ButtonRenderer());
+    table.getColumn("Edit Submission").setCellEditor(new ButtonEditor(new JCheckBox()));
+    JScrollPane scroll = new JScrollPane(table);
+    getContentPane().add(scroll);
+    setSize(500, 200);
+    setVisible(true);
+  }
+  
+  /**
+   * Many slightly different implementations exist on how to add buttons to a table
+   * The code below represents one example of how to do it.
+   * As of now no other implementation in my local test classes works. 
+   * As my understanding of this changes so may the code below.
+   */
+
+  public static void main(String[] args) {
+    AuthorView frame = new AuthorView();
+    frame.addWindowListener(new WindowAdapter() {
+      public void windowClosing(WindowEvent e) {
+        System.exit(0);
+      }
+    });
+  }
+}
+
+class ButtonRenderer extends JButton implements TableCellRenderer {
+
+  public ButtonRenderer() {
+    setOpaque(true);
+  }
+
+  public Component getTableCellRendererComponent(JTable table, Object value,
+      boolean isSelected, boolean hasFocus, int row, int column) {
+    if (isSelected) {
+      setForeground(table.getSelectionForeground());
+      setBackground(table.getSelectionBackground());
+    } else {
+      setForeground(table.getForeground());
+      setBackground(UIManager.getColor("Button.background"));
+    }
+    setText((value == null) ? "" : value.toString());
+    return this;
+  }
+}
+
+class ButtonEditor extends DefaultCellEditor {
+  protected JButton button;
+  private String label;
+  private boolean isPushed;
+
+  public ButtonEditor(JCheckBox checkBox) {
+    super(checkBox);
+    button = new JButton();
+    button.setOpaque(true);
+    button.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        fireEditingStopped();
+      }
+    });
+  }
+
+  public Component getTableCellEditorComponent(JTable table, Object value,
+      boolean isSelected, int row, int column) {
+    if (isSelected) {
+      button.setForeground(table.getSelectionForeground());
+      button.setBackground(table.getSelectionBackground());
+    } else {
+      button.setForeground(table.getForeground());
+      button.setBackground(table.getBackground());
+    }
+    label = (value == null) ? "" : value.toString();
+    button.setText(label);
+    isPushed = true;
+    return button;
+  }
+
+  public Object getCellEditorValue() {
+    if (isPushed) {
+      JOptionPane.showMessageDialog(button, label + " was pressed!");
+    }
+    isPushed = false;
+    return new String(label);
+  }
+
+  protected void fireEditingStopped() {
+    super.fireEditingStopped();
+  }
 }
