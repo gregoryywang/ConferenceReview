d201377015c32a86bfa2a0a3488a501cea23ab99
Yong Yu Wang <yongyuwang@users.noreply.github.com>
2013-05-26 01:13:37 +0000

    fixed the problem of the dissapearing main method.

--- src/gui/AuthorView.java
+++ src/gui/AuthorView.java
@@ -50,7 +50,7 @@ public class AuthorView extends JFrame {
 		setVisible(true);
 	}
 
- 	static void main(String[] args) {
+ 	public static void main(String[] args) {
 	  
  		User testUser = new User();
  		AuthorView frame = new AuthorView(testUser);
