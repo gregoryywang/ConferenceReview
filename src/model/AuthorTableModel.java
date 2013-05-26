7be08cdc452ac7413941e42b5a3cb5df7cf121d7
yongyuwang5@gmail.com <yongyuwang5@gmail.com@e314a958-d412-a368-9749-d0e056828d1e>
2013-05-26 00:39:51 +0000

    -Added debug flags to AuthorView to keep track of button data -Created AuthorTableModel, will perform the function of populating the JTable in AuthorView.

---
+++ src/model/AuthorTableModel.java
@@ -0,0 +1,32 @@
+package model;
+
+/**
+ * AuthorTableModel.java
+ * @author yongyuwang
+ * @version 5-25-1736
+ * This is the table model that will be used by the JTable
+ * in AuthorView.
+ */
+import javax.swing.table.AbstractTableModel;
+
+public class AuthorTableModel extends AbstractTableModel  {
+
+	@Override
+	public int getColumnCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getRowCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
