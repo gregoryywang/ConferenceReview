f7ec65561d890b298d15547bfbacd4903bea1ce8
levonk@uw.edu <levonk@uw.edu@e314a958-d412-a368-9749-d0e056828d1e>
2013-05-27 02:18:09 +0000

    TCSS 360 Software Development & Quality Assurance 26 May, 2013 Group Project
    Finished:
    
    - finished updating method calls in the ConferenceForm class
    - finished adding a cancel button to the ConferenceForm class' panel
    - finished adding some error checking when trying to
    save an invalid conference Date in the ConferenceForm class
    
    To Do:
    
    - continue updating the method calls to the ConferenceForm class
    when they become available

--- src/gui/SubPGChairView.java
+++ src/gui/SubPGChairView.java
@@ -1,5 +1,79 @@
 package gui;
 
-public class SubPGChairView {
+import java.awt.Color;
+import java.awt.Dimension;
+import java.util.List;
 
+import javax.swing.JFrame;
+
+import model.Paper;
+import model.Review;
+import model.SubProgramChair;
+import model.Viewer;
+
+public class SubPGChairView extends JFrame implements Viewer
+{
+	/**
+	 * The default serial version UID.
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * The default frame width.
+	 */
+	private static final int FRAME_WIDTH = 500;
+	
+	/**
+	 * The default frame height.
+	 */
+	private static final int FRAME_HEIGHT = 300;
+	
+	/**
+	 * The default background color.
+	 */
+	private static final Color BACKGROUND_COLOR = Color.MAGENTA;
+	
+	/**
+	 * Reference to my SubProgramChair Object.
+	 */
+	private SubProgramChair my_subprogram_chair;
+	
+	/**
+	 * Constructs a new SubPGhairView Object.
+	 */
+	public SubPGChairView()
+	{
+		super("Sub-Program Chair");
+		setPreferredSize(new Dimension(FRAME_WIDTH, FRAME_HEIGHT));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBackground(BACKGROUND_COLOR);
+		my_subprogram_chair = new SubProgramChair();
+	}
+	
+	public void start()
+	{
+		setVisible(true);
+		pack();
+	}
+
+	@Override
+	public List<Paper> viewPapers() {
+		return null;
+	}
+
+	@Override
+	public List<Review> viewReviews() {
+		return null;
+	}
+	
+	/**
+	 * Main method that will be used for testing the SubPGChairView.
+	 * 
+	 * @param the_args the command-line arguments
+	 */
+	public static void main(final String[] the_args)
+	{
+		new SubPGChairView().start();
+	}
+	
 }
