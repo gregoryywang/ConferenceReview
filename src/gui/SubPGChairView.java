dafda41ea38245c1d652b55a826655c2ed50defe
levonk@uw.edu <levonk@uw.edu>
2013-05-27 03:34:05 +0000

    TCSS 360 - Software Development & Quality Assurance 26 May, 2013 Group Project
    Finished: 
    
    - finished creating a basic outline for the SubProgramChair class
    - finished creating a basic outline for the SubPGChairView class
    
    To do:
    
    - create the RecommendationForm class for the SubPGChairView class
    - create the basic GUI for the SubPGChairView class

--- src/gui/SubPGChairView.java
+++ src/gui/SubPGChairView.java
@@ -2,10 +2,12 @@ package gui;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.JFrame;
 
+import model.Conference;
 import model.Paper;
 import model.Review;
 import model.SubProgramChair;
@@ -34,9 +36,14 @@ public class SubPGChairView extends JFrame implements Viewer
 	private static final Color BACKGROUND_COLOR = Color.MAGENTA;
 	
 	/**
-	 * Reference to my SubProgramChair Object.
+	 * Reference to the SubProgramChair Object associated with this view.
 	 */
-	private SubProgramChair my_subprogram_chair;
+	private SubProgramChair my_user;
+	
+	/**
+	 * Reference to the Collection of Paper Objects associated with this User.
+	 */
+	private List<Paper> my_papers;
 	
 	/**
 	 * Constructs a new SubPGhairView Object.
@@ -47,27 +54,82 @@ public class SubPGChairView extends JFrame implements Viewer
 		setPreferredSize(new Dimension(FRAME_WIDTH, FRAME_HEIGHT));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBackground(BACKGROUND_COLOR);
-		my_subprogram_chair = new SubProgramChair();
+		my_user = new SubProgramChair(new Conference(),"first", "last", "password", "screenName", "me@apple.com");
+		my_papers = new ArrayList<Paper>();
 	}
 	
+	/**
+	 * Initializes the SubProgramChair GUI.
+	 */
 	public void start()
 	{
 		setVisible(true);
 		pack();
 	}
-
+	
+	/**
+	 * Assigns a Recommendation to a specified Paper.
+	 * 
+	 * @param the_paper the Paper that the
+	 * Recommendation is being assigned to
+	 */
+	public void writeRecommendation(final Paper the_paper)
+	{
+		
+	}
+	
+	/**
+	 * View the Reviews associated with the specified Paper.
+	 * 
+	 * @param the_paper the Paper that
+	 * the User is requesting Reviews for
+	 */
+	public List<Review> viewReview(final Paper the_paper)
+	{
+		return null;
+	}
+	
+	/**
+	 * Assigns a Reviewer to a specified Paper.
+	 * 
+	 * @param the_paper the Paper that
+	 * the Reviewer is being assigned to
+	 */
+	public void assignReviewer(final Paper the_paper)
+	{
+		
+	}
+	
+	/**
+	 * The graphical representation of the
+	 * specified paper.
+	 * 
+	 * @param the_paper the Paper that the User
+	 * is Requesting to View
+	 */
+	public Paper viewPaper(final Paper the_paper)
+	{
+		return null;
+	}
+	
+	/**
+	 * Overrides the implemented Viewer method.
+	 */
 	@Override
 	public List<Paper> viewPapers() {
 		return null;
 	}
 
+	/**
+	 * Overrides the implemented Viewer method.
+	 */
 	@Override
 	public List<Review> viewReviews() {
 		return null;
 	}
 	
 	/**
-	 * Main method that will be used for testing the SubPGChairView.
+	 * Main method to test the SubPGChairView.
 	 * 
 	 * @param the_args the command-line arguments
 	 */
--- src/model/SubProgramChair.java
+++ src/model/SubProgramChair.java
@@ -1,24 +1,72 @@
 package model;
 
+import service.PaperService;
+import dao.UserDAO;
+
+/**
+ * Class to create a SubProgramChair Object and its functionalities.
+ * 
+ * @author Levon Kechichian
+ * @version Spring 2013
+ */
 public class SubProgramChair extends User
 {
-	public SubProgramChair()
+	// Hmmmmm.....do I need any fields?
+	
+	/**
+	 * Constructs a SubProgramChair Object based
+	 * on the given parameters.
+	 * 
+	 * @param the_conference the Conference the User is a SubProgramChair for
+	 * @param the_first_name the first name of the User
+	 * @param the_last_name	the last name of the User
+	 * @param the_password the Users password
+	 * @param the_username the Users screen name
+	 * @param the_email the Users e-mail
+	 */
+	public SubProgramChair(final Conference the_conference, final String the_first_name, final String the_last_name, 
+		final String the_password, final String the_username, final String the_email)
 	{
-		
+		super(the_conference, Role.SUB_PROGRAM_CHAIR, the_first_name, the_last_name, the_password, the_username, the_email);
 	}
 	
+	/**
+	 * Assigns a Reviewer to a Collection of Reviewers.
+	 * 
+	 * @param the_user the User that is being assigned to the Collection
+	 */
 	public void assignReviewerToPool(final User the_user)
 	{
-		
+		// What the ????
+		// How is this method different than the one below????
+		// I am assuming we just add the_user to the database of Users?
+		// and I will code it that way...
+		new UserDAO().getUsers(Role.REVIEWER).add(the_user);
 	}
 	
+	/**
+	 * Assigns a Reviewer to a specified Paper.
+	 * 
+	 * @param the_user the Reviewer that is being assigned to the Paper
+	 * @param the_paper the Paper that the User is being assigned to
+	 */
 	public void assignReviewer(final User the_user, final Paper the_paper)
 	{
-		
+		//We need a assignReviewer method in the PaperService class
+		//The following will work to assign a Reviewer to the Paper until that method is created
+		final Review user_review = new Review(the_user);
+		user_review.setReviewer(the_user.getID());
+		PaperService.getInstance().addReview(user_review, the_paper.getID());
 	}
 	
-	public void submitRecommendation(final Paper the_paper)
+	/**
+	 * Submits a Recommendation for a specified Paper.
+	 * 
+	 * @param the_paper the Paper the Recommendation is being
+	 * assigned to
+	 */
+	public void submitRecommendation(final Recommendation the_recommendation, final Paper the_paper)
 	{
-		
+		PaperService.getInstance().addRecommendation(the_recommendation, the_paper.getID());
 	}
 }
