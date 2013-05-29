5049073dd1c6cbda7316ab58af26ddc0adb95903
yongyuwang5@gmail.com <yongyuwang5@gmail.com@e314a958-d412-a368-9749-d0e056828d1e>
2013-05-29 00:32:52 +0000

    The AuthorTableModel is finished. -It has successfully passed testing in displaying a paper in the list. -Whether or not the DAO part of getting this list works is unknown.

--- src/model/AuthorTableModel.java
+++ src/model/AuthorTableModel.java
@@ -4,10 +4,15 @@ package model;
  * AuthorTableModel.java
  * @author yongyuwang
  * @version 5-28-1632
- * This is the table model that will be used by the JTable
+ * This is the table model that will be used by the JTable.
  * in AuthorView.
  */
+
 import javax.swing.table.AbstractTableModel;
+
+import java.util.List;
+
+import service.PaperService;
 
 /*
  * REMINDER: ROW/COLUMN COUNT STARTS AT 0!
@@ -15,23 +20,82 @@ import javax.swing.table.AbstractTableModel;
 
 public class AuthorTableModel extends AbstractTableModel  {
 	
+	/**
+	 * Default serial ID
+	 */
+	private static final long serialVersionUID = 1L;
+
 	// hard coded column names in table
 	private String[] columnNames = { "Title", "Catagory", "Status", "Edit" };
 	
 	// data to be extracted from papers in the database
-	// private Object[][] data;
+	private Object[][] data;
 	
 	// DEBUG: hard coded test data table
-	private Object[][] data = {{ "Paper 1 Name", "Paper 1 Status", "Catagory 1", "Edit 1" }, { "Paper 2 Name", "Paper 2 Status", "catagory 2", "Edit 2" }};
+	// private Object[][] data = {{ "Paper 1 Name", "Paper 1 Status", "Category 1", "Edit 1" }, 
+	//							  { "Paper 2 Name", "Paper 2 Status", "Category 2", "Edit 2" }};
+	// DEBUG: uncomment line below to test
+	// List<Paper> testList;
 	
-	public AuthorTableModel(User user) {
+	private User user;
+	
+	public AuthorTableModel(User the_user) {
 		super();
+		this.user = the_user;
+		
+		// DEBUG: uncommment line below to test
+		// test();
+		
+		getData();
 	}
+	
+	/** DEBUG
+	public void test() {
+		testList = new ArrayList();
+		Paper test_paper = new Paper();
+		User test_user = new User();
+		test_user.setID(1);
+		test_paper.setAuthor(test_user);
+		test_paper.setTitle("Testing Paper Stuff.");
+		test_paper.setCategory("Software");
+		test_paper.setKeywords("test, greatness");
+		test_paper.setAbstract("Wow, I can write an abstract.");
+		testList.add(test_paper);
+	}
+	*/
 	
 	public void getData() {
 		
+		// Gets current conference from user.
+		Conference currentConference = user.getConference();
+		// Retrieves an collection of papers related to this user from the database.
+		List<Paper> paperList = PaperService.getInstance().getAssignedPapers(user.getID(), currentConference.getID(), user.getRole());
+		
+		// DEBUG: uncomment line below to test
+		// List <Paper> paperList = testList;
+		
+		// constructs two dimensional array based on number of papers
+		data = new Object[paperList.size()][columnNames.length];
+		
+		/*
+		 * Each column must match predefined labels in columnNames.
+		 * For the author table, the last column is always the edit button.
+		 */
+		for (int i = 0; i < paperList.size(); i++) {
+			Paper current = paperList.get(i);
+			for (int row = 0; row < paperList.size(); row++) {
+				data[row][0] = current.getTitle();
+				data[row][1] = current.getCategory();
+				data[row][2] = current.getStatus();
+				data[row][3] = "Edit Submission";
+			}
+		}
 	}
 
+	
+	/*
+     * Returns the number of columns in the table model. 
+     */
 	@Override
 	public int getColumnCount() {
 		return columnNames.length;
@@ -45,18 +109,25 @@ public class AuthorTableModel extends AbstractTableModel  {
         return columnNames[column];
     }
 
+    /*
+     * Returns the number of rows in the table model. 
+     */
 	@Override
 	public int getRowCount() {
 		return data.length;
 	}
 
+	/*
+     * Returns the value of a table model at the specified 
+     * row index and column index. 
+     */
 	@Override
 	public Object getValueAt(int rowIndex, int columnIndex) {
 		return data[rowIndex][columnIndex];
 	}
 	
 	 /*
-     * The column that the edit button is located needs to have editable cells!
+     * The column that the edit button is located in needs to have editable cells!
      * Otherwise the edit button does not work.
      */
     public boolean isCellEditable(int row, int col) {
