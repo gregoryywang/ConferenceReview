967e16cc29ec65a82409df862d4c50b72e4c9302
dqt@uw.edu <dqt@uw.edu@e314a958-d412-a368-9749-d0e056828d1e>
2013-05-29 23:45:07 +0000

    Revert back to older version which was working just fine.

--- src/gui/AdminView.java
+++ src/gui/AdminView.java
@@ -1,23 +1,24 @@
 package gui;
 
 import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 import model.Administrator;
 import model.Conference;
 import model.Paper;
 import model.Review;
+import model.Role;
 import model.User;
 import model.Viewer;
 import service.ConferenceService;
@@ -41,7 +42,7 @@ public class AdminView extends JPanel implements Viewer
 	 * Reference to the Administrator Object
 	 * associated with this View.
 	 */
-	private User my_administrator;
+	private Administrator my_administrator;
 	
 	/**
 	 * Creates a default AdminView Object.
@@ -62,53 +63,59 @@ public class AdminView extends JPanel implements Viewer
 	{
 		super(new BorderLayout());
 		my_administrator = new Administrator(the_admin);
-		add(createPanel(), BorderLayout.CENTER);
+//		add(createCenterPanel(), BorderLayout.CENTER);
+		//System.out.println("ADMV_MSG... " + my_administrator.getRole() + " " + my_administrator.getEmail() +" "+ my_administrator.getConference());
+		new ConferenceForm(this, my_administrator.getConference()).start(); //Danielle
 	}
-	
+
 	/**
 	 * Creates the center panel for the AdminView Frame.
 	 * 
 	 * @return returns the populated central panel
 	 */
-	private JPanel createPanel()
+	private JPanel createCenterPanel()
 	{
 		final JPanel panel = new JPanel(new GridLayout(0, 1));
 		
-		final JPanel new_conference_panel = new JPanel();
-		final JButton new_conference_button = new JButton("CREATE A NEW CONFERENCE");
+		final JPanel new_conference_panel = new JPanel(new FlowLayout());
+		final JButton new_conference_button = new JButton("CREATE NEW CONFERENCE");
 		new_conference_button.addActionListener(new ActionListener()
 		{
 			public void actionPerformed(final ActionEvent the_event)
 			{
-				JOptionPane.showMessageDialog(null, "So you think you can create a conference?");
-				final ConferenceForm form = new ConferenceForm(my_administrator, new Conference());
+				final AdminView view = new AdminView();
+				User pgrm_chair = new User();
+				pgrm_chair.setFirstName("NEWWWWWW PGCHAAAAAIIIIRRR");
+				final Conference conference = new Conference(10, new Date(System.currentTimeMillis()),
+					pgrm_chair, "FInished", new Date(System.currentTimeMillis()), 
+					new Date(System.currentTimeMillis()), new Date(System.currentTimeMillis()), 
+					new Date(System.currentTimeMillis()), new Date(System.currentTimeMillis()));
+				
+				final ConferenceForm form = new ConferenceForm(view, conference);
 				form.start();
-				createPanel();
 			}
 		});
 		new_conference_panel.add(new_conference_button);
 		panel.add(new_conference_panel);
 		
-		final JPanel conference_panel = new JPanel();
-		conference_panel.add(new JLabel("Select a Conference: "));
-		final JComboBox conferences = new JComboBox(new DefaultComboBoxModel(
-			ConferenceService.getInstance().getConferences().toArray()));
-		conferences.setEditable(false);
-		conference_panel.add(conferences);
-		final JButton conference_button = new JButton("Go");
-		conference_button.addActionListener(new ActionListener()
+		final JPanel center_panel = new JPanel(new FlowLayout());
+		center_panel.add(new JLabel("\t Conferences "));
+		final JComboBox conference_combo_box = new JComboBox(
+			ConferenceService.getInstance().getConferences().toArray());
+		center_panel.add(conference_combo_box);
+		final JButton combo_box_button = new JButton("Go"); 
+		combo_box_button.addActionListener(new ActionListener()
 		{
 			public void actionPerformed(final ActionEvent the_event)
 			{
-				JOptionPane.showMessageDialog(null, "Don't click me!");
-				final Conference chosen_conference = (Conference) conferences.getSelectedItem();
-				final ConferenceForm form = new ConferenceForm(my_administrator, chosen_conference);
-				form.start();
-				createPanel();
+				final Conference conference_item = (Conference) conference_combo_box.getSelectedItem();
+				new ConferenceForm((AdminView)the_event.getSource(), conference_item).start();
+//						createConferenceForm(ConferenceService.getInstance().
+//					getConference(conference_item.getID()));
 			}
 		});
-		conference_panel.add(conference_button);
-		panel.add(conference_panel);
+		center_panel.add(combo_box_button);
+		panel.add(center_panel);
 		
 		return panel;
 	}
@@ -146,18 +153,20 @@ public class AdminView extends JPanel implements Viewer
 	  return new ArrayList<Review>();
 	}
 	
-//	/**
-//	 * Method to test the AdminView class.
-//	 * 
-//	 * @param the_args the command-line args
-//	 */
-//	public static void main(final String[] the_args)
-//	{
-//		final JFrame frame = new JFrame("AdminView");
-//		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//		final AdminView admin_view = new AdminView();
-//		frame.add(admin_view);
-//		frame.pack();
-//		frame.setVisible(true);
-//	}
+	/**
+	 * Method to test the AdminView class.
+	 * 
+	 * @param the_args the command-line args
+	 */
+	/*
+	public static void main(final String[] the_args)
+	{
+		final JFrame frame = new JFrame("AdminView");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		final AdminView admin_view = new AdminView();
+		admin_view.fillPanel();
+		frame.add(admin_view);
+		frame.pack();
+		frame.setVisible(true);
+	}*/
 }
