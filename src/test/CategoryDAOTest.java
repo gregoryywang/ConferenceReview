e515f37a980c3b8a9894792369498af97993dd95
dqt@uw.edu <dqt@uw.edu@e314a958-d412-a368-9749-d0e056828d1e>
2013-06-03 19:43:07 +0000

    Finished CategoryDAOTest and ConferenceDAOTest

--- src/test/CategoryDAOTest.java
+++ src/test/CategoryDAOTest.java
@@ -12,7 +12,8 @@ import dao.CategoryDAO;
 /**
  * Tests for CategoryDAO class.
  * @author Roshun Jones
- * @version 1.0
+ * @author Danielle
+ * @version 2013 Spring (done)
  */
 public class CategoryDAOTest {
   /**
@@ -20,11 +21,31 @@ public class CategoryDAOTest {
    */
   private CategoryDAO categoryDao;
   
-  public CategoryDAOTest() {}
-  
-  
   @Before
   public void setUp() {
     categoryDao = new CategoryDAO();
+  }
+  
+  /**
+   * Test getting all Categories
+   */
+  @Test
+  public void testGetCategories()
+  {
+	  List<String> categories = categoryDao.getCategories();
+	  assertTrue(categories.contains("Other"));
+	  assertTrue(categories.contains("Hardware"));
+	  assertTrue(categories.size() == 4);
+  }
+  
+  /**
+   * Test getting category id from the string of the category.
+   */
+  @Test
+  public void testGetCategory()
+  {
+	  assertEquals(0, categoryDao.getCategory("Hosta"));
+	  assertEquals(1, categoryDao.getCategory("Curriculum"));
+	  assertEquals(2, categoryDao.getCategory("Software"));
   }
 }
