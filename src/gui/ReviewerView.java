0f56aa54a12b27226847ed29561c349372c409a6
Yong Yu Wang <yongyuwang@users.noreply.github.com>
2013-06-03 17:39:20 +0000

    Refactored review view to match current state of the GUI classes. Whoever did the old version, sorry I wiped your code!

--- src/gui/ReviewerView.java
+++ src/gui/ReviewerView.java
@@ -1,38 +1,97 @@
 package gui;
 
+import gui.TablePanel.TableModel;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 import model.Paper;
-import model.Review;
 import model.Reviewer;
 import model.User;
+import service.ConferenceService;
+import service.UserService;
+import controller.Controller;
 
 public class ReviewerView extends JPanel
 {
-	private User my_user;
-	
-	private Review my_review;
-	
-	private Paper my_paper;
-	
-	public ReviewerView()
+
+	/**
+	 * The default serial ID
+	 */
+	private static final long serialVersionUID = 1L;
+	private Reviewer my_user;
+	private TablePanel tablePanel;
+	private ReviewerController controller;
+
+	public ReviewerView(final User the_user) 
 	{
-		super();
-		my_user = new Reviewer(new User());
-		my_paper = new Paper();
-		my_review = new Review();
+		my_user = new Reviewer(the_user);
+		setLayout(new BorderLayout(0, 0));
+		controller = new ReviewerController(my_user);
+
+		String[][] properties = {
+				{"java.lang.String", "Title", "Title", "false"},
+				{"javax.swing.JButton", "Status", "Status", "true"},
+				{"javax.swing.JButton", "Review", "Review", "true"},
+		};
+		// Create table panel.
+		tablePanel = new TablePanel<Paper>(properties, controller);
+
+		// Populate model
+		tablePanel.setModel(my_user.viewPapers());
+
+		add(tablePanel, BorderLayout.NORTH);
+
 	}
-	
-	public ReviewerView(final User the_user, final Paper the_paper)
-	{
-		super();
-		my_user = the_user;
-		my_paper = the_paper;
-		my_review = ((Reviewer) the_user).getReview(the_paper.getID());
+
+	/**
+	 * Returns view's model.
+	 * 
+	 * @return The view's model.
+	 */
+	public TableModel getModel() {
+		return tablePanel.getModel();
 	}
-	
-	public void start()
-	{
+
+	/**
+	 * Test code to launch a local panel.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
 		
 	}
+
+	private class ReviewerController implements Controller, ActionListener
+	{
+
+		public ReviewerController(Reviewer the_reviewer) {
+			
+		}
+
+		@Override
+		public void update(Object aObject) {
+
+		}
+
+		@Override
+		public void setModel(Object aObject) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+
+		}
+	}
+	
+	
 }
+
+
