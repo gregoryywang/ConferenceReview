24c6198182a6fd576c994727b19400c340e90746
yongyuwang5@gmail.com <yongyuwang5@gmail.com@e314a958-d412-a368-9749-d0e056828d1e>
2013-06-07 21:04:31 +0000

    -Checked in code for controller to drive the new view

---
+++ src/controller/RevisedAuthorViewController.java
@@ -0,0 +1,71 @@
+package controller;
+
+import gui.MainView;
+import gui.RevisedAuthorView;
+import gui.RevisedPaperSubmissionForm;
+
+
+import java.awt.event.ActionEvent;
+import java.util.List;
+
+
+import model.Author;
+import model.Paper;
+
+
+public class RevisedAuthorViewController implements Controller
+{
+	private RevisedAuthorView my_view;
+	private List<Paper> model;
+	private Author the_author;
+
+	public RevisedAuthorViewController(final RevisedAuthorView the_view, final List<Paper> the_papers)
+	{
+		my_view = the_view;
+		model = the_papers;
+		the_author = my_view.getAuthor();
+	}
+
+	@Override
+	public void actionPerformed(final ActionEvent the_event)
+	{
+		String command = the_event.getActionCommand();
+		if("view_edit".equals(command))
+		{
+			Paper paper = my_view.getSelectedRow();
+	        new RevisedPaperSubmissionForm(the_author,  model, paper).setVisible(true); 
+		}
+		if("add_submission".equals(command))
+		{
+			//Launches the new paper submission form when pressed.
+	        new RevisedPaperSubmissionForm(the_author, model, new Paper()).setVisible(true); 
+		}
+		if("view_reviews".equals(command))
+		{
+
+		}
+		if("delete_submission".equals(command))
+		{
+			Paper paper = my_view.getSelectedRow();
+			try {
+				the_author.deletePaper(paper);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	
+	}
+
+	@Override
+	public void update(Object aObject) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setModel(Object aObject) {
+		// TODO Auto-generated method stub
+
+	}
+}
