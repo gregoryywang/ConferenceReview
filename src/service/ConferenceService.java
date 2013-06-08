2c72987dee38a6269786215d95e1620188169de6
Danielle Tucker <danielle@tuckerlabs.com>
2013-06-08 00:20:18 +0000

    Updating javadoc comments so everything is commented.

--- src/service/ConferenceService.java
+++ src/service/ConferenceService.java
@@ -7,56 +7,71 @@ import model.Conference;
 import dao.CategoryDAO;
 import dao.ConferenceDAO;
 
+/**
+ * Service the access and modification of conference objects.
+ * @author Roshun
+ * @version 2013 Spring
+ */
 public class ConferenceService {
-  
-  private ConferenceDAO conferenceDao;
-  private CategoryDAO categoryDao;
-  
-  private ConferenceService() {
-    conferenceDao = new ConferenceDAO();
-    categoryDao = new CategoryDAO();
-  }
-  
-  /**
-   * Saves conference object to data source.
-   * @param aConference The conference object to save.
-   */
-  public void saveConference(final Conference aConference) {
-    conferenceDao.saveConference(aConference);
-  }
-  
-  /**
-   * Returns a list of conferences.
-   * @return a list of conferences.
-   * @author Danielle (refactor)
-   */
-  public List<Conference> getConferences() {
-    return conferenceDao.getConferences();
-  }
-  
-  /**
-   * Returns a single conference.
-   * @param the_conf_id the id of the conference.
-   */
-  public Conference getConference(final int the_conf_id) {
-	  Conference conf = conferenceDao.getConference(the_conf_id);
-    return conf;
-  }
-  
-  /**
-   * Returns a list of all system-wide categories for making a new conference.
-   * @return Returns a list of categories.
-   */
-  public List<String> getCategories() {
-    return categoryDao.getCategories();
-  }
-  
-  
-  /**
-   * Returns ConferenceService instance.
-   * @return Returns ConferenceService instance.
-   */
-  public static ConferenceService getInstance() {
-    return new ConferenceService();
-  }
+
+	/**
+	 * The conference DAO
+	 */
+	private ConferenceDAO conferenceDao;
+
+	/**
+	 * The category DAO
+	 */
+	private CategoryDAO categoryDao;
+
+	/**
+	 * Constructor for the Conference Service
+	 */
+	private ConferenceService() {
+		conferenceDao = new ConferenceDAO();
+		categoryDao = new CategoryDAO();
+	}
+
+	/**
+	 * Saves conference object to data source.
+	 * @param aConference The conference object to save.
+	 */
+	public void saveConference(final Conference aConference) {
+		conferenceDao.saveConference(aConference);
+	}
+
+	/**
+	 * Returns a list of conferences.
+	 * @return a list of conferences.
+	 * @author Danielle (refactor)
+	 */
+	public List<Conference> getConferences() {
+		return conferenceDao.getConferences();
+	}
+
+	/**
+	 * Returns a single conference.
+	 * @param the_conf_id the id of the conference.
+	 */
+	public Conference getConference(final int the_conf_id) {
+		Conference conf = conferenceDao.getConference(the_conf_id);
+		return conf;
+	}
+
+	/**
+	 * Returns a list of all system-wide categories for making a new conference.
+	 * @return Returns a list of categories.
+	 */
+	public List<String> getCategories() {
+		return categoryDao.getCategories();
+	}
+
+
+	/**
+	 * Returns ConferenceService instance.
+	 * @return Returns ConferenceService instance.
+	 */
+	public static ConferenceService getInstance() {
+		return new ConferenceService();
+	}
 }
--- src/service/PaperService.java
+++ src/service/PaperService.java
@@ -13,122 +13,140 @@ import dao.PaperDAO;
 /**
  * Provides services related to papers.
  * @author Roshun Jones
- *
+ * @version 2013 Spring
  */
 public class PaperService {
-  private PaperDAO paperDao;
-  
-  private PaperService() {
-    paperDao = new PaperDAO();
-  }
-  
-  /**
-   * Saves a paper object.
-   * @param aPaper The paper to save.
-   */
-  public void savePaper(final Paper aPaper){
-    paperDao.savePaper(aPaper);
-  }
-  
-  /**
-   * Assigns a paper to a user.
-   */
-  public void assignPaper(final int aPaperId, 
-                          final int aUserId, 
-                          final int aConfId,
-                          final Role aRole) {
-    paperDao.assignPaper(aPaperId, aUserId, aConfId, aRole);
-    
-  }
-  
-  /**
-   * Returns assigned papers.
-   * @param aUserId The assignee's user id.
-   * @param aConfId The conference id.
-   * @param aRole The role.
-   */
-  public List<Paper> getAssignedPapers(final int aUserId, 
-                                       final int aConfId, 
-                                       final Role aRole) {
-	  List<Paper> papers = paperDao.getPapers(aUserId, aRole, aConfId);
-	  for(Paper paper: papers)
-	  {
-		  paper.setReview(paperDao.getReviews(paper.getID()));
-	  }
-    return papers;
-  }
-  
-  /**
-   * Submits paper review.
-   */
-  public void addReview(final Review aReview, final Paper the_paper) 
-  {
-	  
-	  paperDao.addReview(aReview, the_paper.getID());
-  }
-  
-  /**
-   * Get all reviews associated with this paper.
-   * @param aPaperId the ID of the paper
-   * @return All Reviews associated with this paper.  If none exist, an
-   * empty list will be returned.
-   */
-  public List<Review> getReviews(final int aPaperId) {
-    return paperDao.getReviews(aPaperId);
-  }
-  
-  /**
-   * Submits paper recommendation from the SubprogramChair.
-   */
-  public void addRecommendation(final Recommendation aRecommendation, final Paper aPaper) 
-  {
-	  aPaper.setRecommendation(aRecommendation);
-	  paperDao.savePaper(aPaper);
-  }
-  
-  /**
-   * Access the recommendation for this paper
-   * @param aPaperId the id of the paper
-   * @return the recommendation for this paper.  If it does not exist, 
-   * a default Recommendation will be returned.
-   */
-  public Recommendation getRecommendation(final int aPaperId) 
-  {
-	  return paperDao.getRecommendation(aPaperId);
-  }
-  
-  public void deletePaper(final int the_paper_id)
-  {
-	  paperDao.deletePaper(the_paper_id);
-  }
-  
-  /**
-   * Returns PaperService instance.
-   * @return Returns PaperService instance.
-   */
-  public static PaperService getInstance() {
-    return new PaperService();
-  }
-  
-  /**
-   * 
-   * @param the_paper_id the paper id
-   * @return the user which is the subprogram chair for the paper. (If no
-   * subprogram chair exists, the default SubprogramChair will be returned.
-   */
-  public SubProgramChair getAssignedSubprogramChair(final int the_paper_id)
-  {
-	  return paperDao.getAssignedSubProgramChair(the_paper_id);
-  }
-  
-  /**
-   * 
-   * @param the_paper_id
-   * @return the list of Reviewers associated with the paper. If no reviewers
-   * are assigned, then returns an empty list.
-   */
-  public List<Reviewer> getAssignedReviewers(final int the_paper_id)
-  {
-	  return paperDao.getAssignedReviewers(the_paper_id);
-  }
+	/**
+	 * The paper dao
+	 */
+	private PaperDAO paperDao;
+
+	/**
+	 * Create a paper service object.
+	 */
+	private PaperService() {
+		paperDao = new PaperDAO();
+	}
+
+	/**
+	 * Saves a paper object.
+	 * @param aPaper The paper to save.
+	 */
+	public void savePaper(final Paper aPaper){
+		paperDao.savePaper(aPaper);
+	}
+
+	/**
+	 * Assigns/associates a paper to a user, within a conference, and for a particular role.
+	 * @param aPaperId the id of the paper
+	 * @param aUserId the id of the user
+	 * @param aConfId the id of the conference
+	 * @param aRole the role of the user who is to be associated with the paper.
+	 */
+	public void assignPaper(final int aPaperId, 
+			final int aUserId, 
+			final int aConfId,
+			final Role aRole) {
+		paperDao.assignPaper(aPaperId, aUserId, aConfId, aRole);
+
+	}
+
+	/**
+	 * Returns assigned papers.
+	 * @param aUserId The assignee's user id.
+	 * @param aConfId The conference id.
+	 * @param aRole The role.
+	 */
+	public List<Paper> getAssignedPapers(final int aUserId, 
+			final int aConfId, 
+			final Role aRole) {
+		List<Paper> papers = paperDao.getPapers(aUserId, aRole, aConfId);
+		for(Paper paper: papers)
+		{
+			paper.setReview(paperDao.getReviews(paper.getID()));
+		}
+		return papers;
+	}
+
+	/**
+	 * Submits paper review.  Also adds the review to the paper object.
+	 * @param aReview the review to add to the paper
+	 * @param the_paper the paper to associate with this review
+	 */
+	public void addReview(final Review aReview, final Paper the_paper) 
+	{
+		the_paper.addReview(aReview);
+		paperDao.addReview(aReview, the_paper.getID());
+	}
+
+	/**
+	 * Get all reviews associated with this paper.
+	 * @param aPaperId the ID of the paper
+	 * @return All Reviews associated with this paper.  If none exist, an
+	 * empty list will be returned.
+	 */
+	public List<Review> getReviews(final int aPaperId) {
+		return paperDao.getReviews(aPaperId);
+	}
+
+	/**
+	 * Submits paper recommendation from the SubprogramChair.
+	 * @param aRecommendation the recommendation
+	 * @param aPaper the paper
+	 */
+	public void addRecommendation(final Recommendation aRecommendation, final Paper aPaper) 
+	{
+		aPaper.setRecommendation(aRecommendation);
+		paperDao.savePaper(aPaper);
+	}
+
+	/**
+	 * Access the recommendation for this paper
+	 * @param aPaperId the id of the paper
+	 * @return the recommendation for this paper.  If it does not exist, 
+	 * a default Recommendation will be returned.
+	 */
+	public Recommendation getRecommendation(final int aPaperId) 
+	{
+		return paperDao.getRecommendation(aPaperId);
+	}
+
+	/**
+	 * Delete the paper from active status
+	 * @param the_paper_id the paper id
+	 */
+	public void deletePaper(final int the_paper_id)
+	{
+		paperDao.deletePaper(the_paper_id);
+	}
+
+	/**
+	 * Get the assigned subprogram chair for this paper
+	 * @param the_paper_id the paper id
+	 * @return the user which is the subprogram chair for the paper. (If no
+	 * subprogram chair exists, the default SubprogramChair will be returned.
+	 */
+	public SubProgramChair getAssignedSubprogramChair(final int the_paper_id)
+	{
+		return paperDao.getAssignedSubProgramChair(the_paper_id);
+	}
+
+	/**
+	 * Get a list of all assigned reviewers for this paper.
+	 * @param the_paper_id
+	 * @return the list of Reviewers associated with the paper. If no reviewers
+	 * are assigned, then returns an empty list.
+	 */
+	public List<Reviewer> getAssignedReviewers(final int the_paper_id)
+	{
+		return paperDao.getAssignedReviewers(the_paper_id);
+	}
+
+	/**
+	 * Returns PaperService instance.
+	 * @return Returns PaperService instance.
+	 */
+	public static PaperService getInstance() {
+		return new PaperService();
+	}
 }
